f7b8f54e7f81e056703caaac885e632e553d8c22
Iviss2580 <iviss036@hotmail.com>
2022-07-29 18:44:42 -0300

    agregado firebase y cambiourl

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -9,7 +9,7 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
   providedIn: 'root'
 })
 export class AuthService {
-  authURL = 'http://localhost:8080/auth/';
+  authURL = 'https://agile-brook-70145.herokuapp.com';
 
   constructor(private httpClient: HttpClient) { }
 
--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -7,7 +7,7 @@ import { persona } from '../model/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-  URL = 'http://localhost:8080/personas/';
+  URL = 'https://agile-brook-70145.herokuapp.com';
 
   constructor(private http: HttpClient) { }
   public getPersona(): Observable<persona>{
